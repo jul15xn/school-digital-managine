482f989b5054c641baf7cd3f38ce9adc5ce3335e
Jul1xn <88789943+jul15xn@users.noreply.github.com>
2024-11-28 13:11:02 +0100

    python website website modified and removed article

--- js/article.js
+++
@@ -1,7 +0,0 @@
-const highlightedItems = document.querySelectorAll("img");
-
-highlightedItems.forEach((userItem) => {
-  userItem.addEventListener("click", function() {
-    window.open(userItem.src, "_blank");
-  })
-});

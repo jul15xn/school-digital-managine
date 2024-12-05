a60b0476af9d9c6de503325adb40577cc870c7e9
Joran Nap <143212549+twin1nap@users.noreply.github.com>
2024-12-05 13:49:32 +0100

    tutorial gemaakt joran

---
+++ tutorials/python/test.py
@@ -0,0 +1,18 @@
+# for loop met array:
+array = [1, 2, 3, 4]
+for index in array:
+    print(index)
+
+#for loop met text:
+for letters in "Hello, World!":
+    print(letters)
+
+# for met range:
+for index in range(1, 5):
+    print(index)
+
+
+def test_function(text):
+    print(text)
+
+test_function("Hello, World!")
